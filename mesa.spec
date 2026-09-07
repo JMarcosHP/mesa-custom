JMarcosHP/mesa-custom

commit f4a648302c1e13d8e35a4dc14f3a62b88759c84e

Enable AMD Anti-Lag Vulkan layer
Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2457852

mesa.spec
@@ -442,7 +442,7 @@ rewrite_wrap_file rustc-hash
   -Dgallium-rusticl=true \
 %endif
   -Dvulkan-drivers=%{?vulkan_drivers} \
-  -Dvulkan-layers=device-select \
+  -Dvulkan-layers=device-select,anti-lag \
   -Dgles1=enabled \
   -Dgles2=enabled \
   -Dopengl=true \

commit 232e02ce03c16a2c588ef5c2bed9e979f805e465

Add AMD Anti-Lag Vulkan layer missing files
Fixes: f4a648302c1e ("Enable AMD Anti-Lag Vulkan layer")
Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2457852

mesa.spec
@@ -692,6 +692,8 @@ ln -s libGLX_mesa.so.0 %{buildroot}%{_libdir}/libGLX_system.so.0
 %{_datadir}/drirc.d/00-lavapipe-defaults.conf
 %{_libdir}/libVkLayer_MESA_device_select.so
 %{_datadir}/vulkan/implicit_layer.d/VkLayer_MESA_device_select.json
+%{_libdir}/libVkLayer_MESA_anti_lag.so
+%{_datadir}/vulkan/implicit_layer.d/VkLayer_MESA_anti_lag.json
 %if 0%{?with_virtio}
 %{_libdir}/libvulkan_virtio.so
 %{_datadir}/vulkan/icd.d/virtio_icd.*.json